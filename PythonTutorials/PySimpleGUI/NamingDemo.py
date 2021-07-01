5c533dea963509ad4b421634495cedc2a8470095
George Maynard <galphonsemaynard@gmail.com>
2021-07-01 18:14:20 -0400

    correct syntax for join, needs to be updated with all elements

--- PythonTutorials/PySimpleGUI/NamingDemo.py
+++ PythonTutorials/PySimpleGUI/NamingDemo.py
@@ -8,30 +8,30 @@ sg.theme('NeutralBlue')
 
 ## Create a layout
 layout = [
-  [sg.Text('Global Identifier:'), sg.InputCombo(
-    ['NEFSC','NOAA','External']
-    )],
-  [sg.Text('Grouping identifier (Program Name):'), sg.Input(key="Program")],
-  [sg.Text('Data Explanation (?):'), sg.Input(key="Explain")],
-  [sg.Text('Spatial Scale:'), sg.Input(key="Spatial")],
-  [sg.Text('Temporal Resolution:'),sg.InputCombo(
-    ["Minute","Hourly","Daily","Weekly","Monthly","Annually"]
-    )],
-  [sg.Text('Start Date:'),
-    sg.Input(
-      key='Start', 
-      size=(20,1)), 
-    sg.CalendarButton(
-      'Select Start Date', 
-      close_when_date_chosen=True,  
-      target='Start', 
-      location=(0,0), 
-      no_titlebar=False, 
-      format='%Y-%M-%d'
-    )],
-  [sg.Text('End Month:'),sg.Input(key="End")],
+  ## The global identifier tells the user whether the data was collected by
+  ## NEFSC, NOAA, or an external group
+  [sg.Text('Global Identifier:'), sg.InputCombo(['NEFSC','NOAA','External'])],
+  ## The grouping identifier is a unique project identifier
+  ## I'm not quite sure how to control inputs to this field
+  [sg.Text('Grouping identifier (Program Name):'),sg.Input(key="Program")],
+  ## I assume the data explanation is the type of data being collected, whether
+  ## that's temperature, depth, salinity, DO, etc., but maybe I'm wrong?
+  [sg.Text('Data Explanation (?):'),sg.Input(key="Explain")],
+  ## Not sure how to control this field. By spatial scale, do we mean
+  ##    general: (e.g., local, regional, national, international) or
+  ##    specific: (e.g., NW Atlantic, Gulf of Maine, Great South Channel)
+  ## More information is needed to control inputs on this field
+  [sg.Text('Spatial Scale:'),sg.Input(key="Spatial")],
+  ## Temporal resolution is a drop down menu
+  [sg.Text('Temporal Resolution:'),sg.InputCombo(["Minute","Hourly","Daily","Weekly","Monthly","Annually"])],
+  ## Start date and end date are recorded as date values in the format
+  ## yyyy-mm-dd 
+  [sg.CalendarButton('Select Start Date', close_when_date_chosen=True, target='Start', location=(0,0), no_titlebar=False, format='%Y-%m-%d'),sg.Input(key='Start')],
+  [sg.CalendarButton('Select End Date', close_when_date_chosen=True,  target='End', location=(0,0), no_titlebar=False, format='%Y-%m-%d'),sg.Input(key='End')],
+  ## I'm not quite sure how to control version numbers, I guess that's up to the
+  ## individual project?
   [sg.Text('Version:'),sg.Input(key="v")],
-  [sg.Button('Read'),sg.Exit()]
+  [sg.Exit()]
 ]
 
 ## Create a window
@@ -45,3 +45,4 @@ while True:
     break
   
 window.close()
+"_".join([values[0],values["Program"]])
